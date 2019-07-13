9785d5cfd11f9c8460b2409ff253171e8dc342de
rpeepz <r.papagna.42@gmail.com>
2019-07-12 17:05:17 -0700

    new struct score and try

--- includes/filler.h
+++ includes/filler.h
@@ -16,6 +16,9 @@
 # include "../libft/includes/libft.h"
 # include <sys/types.h>
 # include <stdio.h>
+# define END_PHASE_1 0x1000
+# define END_PHASE_2 0x2000
+# define END_PHASE_3 0x4000
 # define ABS(x) (x < 0 ? -(x) : x)
 # define MAX(a, b) return (a > b ? a : b)
 # define MIN(a, b) return (a < b ? a : b)
@@ -30,7 +33,6 @@ typedef struct		s_point
 {
 	int				x;
 	int				y;
-	int				**delta;
 }					t_point;
 
 typedef struct		s_who
@@ -39,14 +41,6 @@ typedef struct		s_who
 	t_point			start;
 }					t_who;
 
-typedef struct		s_token
-{
-	int				wide;
-	int				tall;
-	int				size;
-	char			**data;
-}					t_token;
-
 typedef struct		s_index
 {
 	int				index;
@@ -54,19 +48,49 @@ typedef struct		s_index
 	struct s_index	*next;
 }					t_index;
 
+typedef struct		s_token
+{
+	int				wide;
+	int				tall;
+	int				size;
+	int				max[2];
+	char			**data;
+}					t_token;
+
+typedef struct		s_try
+{
+	int				score;
+	int				block_count;
+	t_point			anchor;
+	t_point			new_anc;
+	t_point			*block;
+	struct s_try	*next;
+}					t_try;
+
+typedef struct		s_score
+{
+	int				score;
+	t_point			board_point;
+	t_point			piece_point;
+	t_point			target;
+	t_try			*scores;
+	struct s_score	*next;
+}					t_score;
+
 typedef struct		s_game
 {
-	t_who			me;
-	t_who			you;
-	t_token			board;
-	t_token			piece;
-	t_point			target;
+	char			start;
 	int				me_count;
 	int				you_count;
-	char			start;
+	t_point			target;
+	t_who			me;
+	t_who			you;
 	t_point			*me_blocks;
 	t_point			*you_blocks;
+	t_token			board;
+	t_token			piece;
 	t_index			*list;
+	t_score			*scores;
 }					t_game;
 
 /*
@@ -77,20 +101,20 @@ typedef struct		s_game
 
 void				list_join(t_index **head, t_index **alt_list);
 void				diff_mergesort(t_index **headlist);
-void				game_array_push(t_index *list, int index, int diff);
-void				set_token(t_token *token, char *line, int i, int type);
-void				set_blocks(t_game *filler);
-void				set_start(t_game *filler);
+void				game_array_push(t_index **list, int index, int diff);
 void				diff_piece(int *piece, t_game *filler, int i, int j);
 t_index				*sort_diffs(t_game *filler, int i, int j, int mode);
 void				block_find(t_game *filler, t_point anchor, int x, int y);
-void				whose_blocks(t_game *filler);
 void				game_array_rev(t_index **alist);
 int					where_anchor(t_game *filler, int x, int y);
-int					what_axis(t_token piece, int x, int y, int type);
 int					position_valid(t_game filler, t_point point);
 int					dist(t_point a, t_point b);
-void				play_piece(t_game filler);
 int					ft_min(int a, int b);
 int					ft_max(int a, int b);
+
+void				get_phase_one(t_game *filler, int to_match, int mode);
+void				score_list_init(t_game *filler, t_score *score_list, int i);
+void				play_piece(t_game filler);
+void				set_start(t_game *filler);
+void				set_token(t_token *token, char *line, int i, int type);
 #endif
